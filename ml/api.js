ec8da3e3f69cb415ae05030fbb028fa516c3a9cc
fkaita <spkaikai@gmail.com>
2025-01-12 19:17:08 +0900

    add url

--- ml/api.js
+++ ml/api.js
@@ -1,5 +1,7 @@
+const BASE_URL = 'http://127.0.0.1:5000'; 
+
 export async function updateWeightsWithBackend(weights, inputs, targets) {
-    const response = await fetch('/update-weights', {
+    const response = await fetch(`${BASE_URL}/update-weights`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
